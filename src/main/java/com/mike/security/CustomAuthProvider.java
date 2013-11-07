b7272959bf85f21b7af4f73dee087c843e456e5b
mramirez <mramirez@denimgroup.com>
2013-11-07 17:07:52 -0600

    +logging

--- src/main/java/com/mike/security/CustomAuthProvider.java
+++ src/main/java/com/mike/security/CustomAuthProvider.java
@@ -3,6 +3,8 @@ package com.mike.security;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -13,6 +15,8 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 public class CustomAuthProvider implements AuthenticationProvider {
 
+	private Logger log = LoggerFactory.getLogger(this.getClass());
+	
 	@Override
 	public Authentication authenticate(Authentication auth)
 			throws AuthenticationException {
@@ -20,12 +24,16 @@ public class CustomAuthProvider implements AuthenticationProvider {
 		String userName = auth.getPrincipal().toString();
 		String password = auth.getCredentials().toString();
 		
+		log.debug("Request to authenticate, userName: " + userName);
+		
 		if (userName.equals("user") && password.equals("user")){
 			UsernamePasswordAuthenticationToken token;
 			token = new UsernamePasswordAuthenticationToken(userName, password, loadAuthorities());
+			log.info("Authenticated Successful, userName: " + userName);
 			return token;
 		}
 		
+		log.info("Authentication Failed, userName: " + userName);
 		throw new BadCredentialsException("invalid credentials"); 		
 	}
 	
